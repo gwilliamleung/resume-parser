83b833ed1e0cbb497e9b7392c5ef56f1b7d5340c
William Leung <gwilliamleung@gmail.com>
2023-01-28 23:56:29 -0500

    edit

--- src/pages/Search.jsx
+++ src/pages/Search.jsx
@@ -4,17 +4,23 @@ import { UserAuth } from '../context/AuthContext'
 import {updateDoc, doc, onSnapshot} from 'firebase/firestore'
 
 const Search = () => {
-  const {user} = UserAuth()
+  // const {user, firestore} = UserAuth()
+  // const [data, setData] = useState(null);
 
-  useEffect(()=>{
-    onSnapshot(doc(db,"users","user"),(doc)=>{
-      console.log(doc.data());
-    })
-  },[user])
+  // useEffect(() => {
+  //   const unsubscribe = firestore.collection('collectionName').doc('documentId')
+  //     .onSnapshot(doc => {
+  //       setData(doc.data());
+  //       console.log(doc.data());
+  //     });
+  //   return unsubscribe;
+  // }, []);
 
   return (
     <div className="bg-blue-100 flex flex-wrap justify-center  items-center w-full h-screen ">
     <div className="flex flex-col px-28 py-36 bg-white border h-2/3 rounded-lg w-1/2">
+    {/* {data && <p>{data.fieldName}</p>} */}
+
     </div>
   </div>
   )

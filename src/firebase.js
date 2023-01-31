036cbe85188b73da84608fb0421d16e8689af7fd
William Leung <gwilliamleung@gmail.com>
2023-01-30 22:06:24 -0500

    removed app

--- src/firebase.js
+++ src/firebase.js
@@ -8,12 +8,7 @@ import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyCSBiBa-1egFR9jVK0HGsqt2Nceh85Bea8",
-  authDomain: "g-resume-parser.firebaseapp.com",
-  projectId: "g-resume-parser",
-  storageBucket: "g-resume-parser.appspot.com",
-  messagingSenderId: "538346631587",
-  appId: "1:538346631587:web:4cc57d630983813b826a16"
+
 };
 
 // Initialize Firebase
